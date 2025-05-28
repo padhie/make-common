ac823b15fa6eb2918eab1ea6a06248bb5c6d911b
Padhie <padhie@users.noreply.github.com>
2025-05-28 23:50:16 +0200

    Update help.mk

--- help.mk
+++ help.mk
@@ -1,3 +1,3 @@
-help:		## this output
-	@grep -h -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | \
-		awk 'BEGIN {FS = ":.*?## "}; {printf "  %-15s %s\n", $$1, $$2}'
+help:
+	@grep -h -E '^[a-zA-Z0-9_-]+:(.*?## .*)?$$' $(MAKEFILE_LIST) | \
+		awk 'BEGIN {FS = ":(.*?## )?"}; {printf "  %-15s %s\n", $$1, $$2}'
